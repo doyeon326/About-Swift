715f68b2e2119aff97dc9153e05a08c7e2490d79
doyeon326 <doyeon326@gmail.com>
2021-11-09 20:26:16 +0900

    Class vs Structure

---
+++ AboutSwift.playground/Pages/ClassVSStrudture.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class SomeClass {
+    var count: Int = 0
+}
+
+struct SomeStruct {
+    var count: Int = 0
+}
+
+var class1 = SomeClass()
+var class2 = class1
+var class3 = class1
+
+class3.count = 2
+class1.count
+
+
+var struct1 = SomeStruct()
+var struct2 = struct1
+var struct3 = struct1
+
+struct2.count = 3
+struct3.count = 4
+
+struct1.count
+struct2.count
+struct3.count
+//: [Next](@next)
